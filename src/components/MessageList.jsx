6e3b675da612197fb6990204a1a27d56dae0b064
Ryan Filho <ryanfilhoryanfilho@gmail.com>
2017-01-29 23:27:33 -0300

    Changing some things

--- src/components/MessageList.jsx
+++ src/components/MessageList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Message from './Message.jsx';
 import mui from 'material-ui';
+import Firebase from 'firebase'
+import _ from 'lodash'
 
 var {Card, List} = mui;
 
@@ -8,37 +10,31 @@ class MessageList extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            messages:[
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'Hello, how are you?',
-                'I am fine, and you?'
-            ]
+            messages:[]
         };
+
+        // Initialize Firebase
+        var config = {
+            apiKey: "AIzaSyAUIuFex4CKXr062JOG9Ub7l6wzMMQHtDQ",
+            authDomain: "firechat-9ba30.firebaseapp.com",
+            databaseURL: "https://firechat-9ba30.firebaseio.com",
+            storageBucket: "firechat-9ba30.appspot.com",
+            messagingSenderId: "674287501430"
+        };
+        Firebase.initializeApp(config);
     }
+    componentWillMount() {
+        Firebase.database().ref('/messages').once('value').then(function(snapshot) {
+            this.setState({
+                messages: snapshot.val().messages
+            });        
+        });
+    }
+
     render(){
         var messageNodes = this.state.messages.map((message) =>{
             return (
-                <Message message={message} />
+                <Message message={message.text} />
             );
         });
 
